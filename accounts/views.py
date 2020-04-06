aece9e5c4ee64fcfa9ff821f9757cad9bfbfd96b
AdrianPiwnicki <piwnicki.adrian@gmail.com>
2020-04-06 20:08:42 +0200

    porawka v1

--- accounts/views.py
+++ accounts/views.py
@@ -38,6 +38,6 @@ def logout_view(request):
         return redirect('http://77.55.237.205:8000/accounts/login/')
 
 def name_view(request):
-    lista = User.objects.filter(username = request.user)
+    lista = User.objects.order_by('-last_login')
     serializer = UserSerializer(lista, many=True)
-    return JsonResponse(serializer.data, safe=False)
+    return JsonResponse(serializer.data[0], safe=False)
